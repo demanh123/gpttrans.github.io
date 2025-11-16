d235037f1d4352b70b8fe160d27722c87bde62c0
Manh <55310905+demanh123@users.noreply.github.com>
2025-11-17 00:12:30 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,18 @@
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()))
-
-
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
-const url = new URL(event.request.url)
-if (url.pathname === '/share' && event.request.method === 'POST') {
-event.respondWith((async () => {
-const data = await event.request.formData()
-const text = data.get('text') || ''
-
-
-const clients = await self.clients.matchAll({type:'window'})
-if (clients.length) {
-clients[0].postMessage({text})
-clients[0].focus()
-} else {
-self.clients.openWindow('/?t=' + encodeURIComponent(text))
-}
-
-
-return new Response('OK', {status:200})
-})())
-}
-})
+  const url = new URL(event.request.url);
+  if (url.pathname === '/share' && event.request.method === 'POST') {
+    event.respondWith((async () => {
+      const data = await event.request.formData();
+      const text = data.get('text') || '';
+      const clients = await self.clients.matchAll({type:'window'});
+      if (clients.length) {
+        clients[0].postMessage({text});
+        clients[0].focus();
+      } else {
+        self.clients.openWindow('/?t=' + encodeURIComponent(text));
+      }
+      return new Response('OK', {status:200});
+    })());
+  }
+});
