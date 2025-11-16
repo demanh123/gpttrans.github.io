2f227ae6217a2e51ae275357bbc6f744a4b39e0a
Manh <55310905+demanh123@users.noreply.github.com>
2025-11-16 23:54:22 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,24 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => clients.claim());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()))
+
+
+self.addEventListener('fetch', event => {
+const url = new URL(event.request.url)
+if (url.pathname === '/share' && event.request.method === 'POST') {
+event.respondWith((async () => {
+const data = await event.request.formData()
+const text = data.get('text') || ''
+
+
+const clients = await self.clients.matchAll({type:'window'})
+if (clients.length) {
+clients[0].postMessage({text})
+clients[0].focus()
+} else {
+self.clients.openWindow('/?t=' + encodeURIComponent(text))
+}
+
+
+return new Response('OK', {status:200})
+})())
+}
+})
